69a627d964ca79ccb55b909c471a4e44d2ee6545
Sunni Tang <sunnitang@Sunnis-MBP.fios-router.home>
2020-05-08 13:16:35 -0400

    creates WardrobeVC+CollectionViews.swift and TypeCollectionViewCell.swift

---
+++ Potions-Please/Potions-Please/Controllers/WardrobeViewController/WardrobeViewController+CollectionViews.swift
@@ -0,0 +1,31 @@
+//
+//  WardrobeViewController+TypeCollectionView.swift
+//  Potions-Please
+//
+//  Created by Sunni Tang on 5/8/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+extension WardrobeViewController {
+    func setUpCollectionView(collectionView: UICollectionView) {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
+}
+
+extension WardrobeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
+    }
+    
+}
+
+extension WardrobeViewController: UICollectionViewDelegateFlowLayout {
+    
+}
---
+++ Potions-Please/Potions-Please/Views/TypeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TypeCollectionViewCell.swift
+//  Potions-Please
+//
+//  Created by Sunni Tang on 5/8/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+class TypeCollectionViewCell: UICollectionViewCell {
+    
+}
