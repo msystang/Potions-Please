6562af2f9a791032906ba7257b55d41da30c1657
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-05-11 14:26:57 -0400

    changes tabbar colors

--- Potions-Please/Potions-Please/Controllers/PPTabBarController.swift
+++ Potions-Please/Potions-Please/Controllers/PPTabBarController.swift
@@ -16,7 +16,12 @@ class PPTabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        configureTabBar()
+        setTabBarColors()
+    }
+    
+    private func configureTabBar() {
         wardrobeVC.tabBarItem = UITabBarItem(title: "Witch's Wardrobe", image: UIImage(systemName: "wand.and.stars"), tag: 0)
         selfieVC.tabBarItem = UITabBarItem(title: "Selfie", image: UIImage(systemName: "camera"), tag: 1)
         infoVC.tabBarItem = UITabBarItem(title: "Potions Please", image: UIImage(systemName: "info.circle"), tag: 2)
@@ -24,5 +29,9 @@ class PPTabBarController: UITabBarController {
         self.viewControllers = [wardrobeVC, selfieVC, infoVC]
     }
     
+    private func setTabBarColors() {
+        self.tabBar.tintColor = UIColor(red: 188/255, green: 175/255, blue: 209/255, alpha: 1)
+        self.tabBar.barTintColor = UIColor(red: 218/255, green: 214/255, blue: 229/255, alpha: 1)
+    }
 
 }
