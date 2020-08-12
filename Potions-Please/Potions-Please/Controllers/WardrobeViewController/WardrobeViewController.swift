dc0341dd669c56810a54dfaa7bda2f2051fccb03
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-08-12 14:48:54 -0400

    adjusts height constraint for partial sliderView

--- Potions-Please/Potions-Please/Controllers/WardrobeViewController/WardrobeViewController.swift
+++ Potions-Please/Potions-Please/Controllers/WardrobeViewController/WardrobeViewController.swift
@@ -31,7 +31,7 @@ class WardrobeViewController: UIViewController {
     var sliderView = SliderView()
     
     lazy var sliderOpenedHeightConstraint: NSLayoutConstraint = sliderView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.8)
-    lazy var sliderPartialHeightConstraint:NSLayoutConstraint = sliderView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.3)
+    lazy var sliderPartialHeightConstraint:NSLayoutConstraint = sliderView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.27)
     lazy var sliderCollapsedHeightConstraint:NSLayoutConstraint = sliderView.heightAnchor.constraint(equalToConstant: 90)
     
     // MARK: - Internal Properties
