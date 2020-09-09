b781e3235a3f0761374b86f82675a5c34b27a2d5
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-09-09 13:24:16 -0400

    creates button objects and constraints in MenuView.swift

--- Potions-Please/Potions-Please/Views/MenuView/MenuView+Constraints.swift
+++ Potions-Please/Potions-Please/Views/MenuView/MenuView+Constraints.swift
@@ -10,12 +10,60 @@ import UIKit
 
 extension MenuView {
     func addSubviews() {
-
+        self.addSubview(menuButton)
+        self.addSubview(downloadButton)
+        self.addSubview(clearButton)
+        self.addSubview(infoButton)
     }
 
     func addConstraints() {
-        
+        setMenuButtonConstraints()
+        setDownloadButtonConstraints()
+        setClearButtonConstraints()
+        setInfoButtonConstraints()
     }
     
-
+    private func setMenuButtonConstraints() {
+        menuButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            menuButton.heightAnchor.constraint(equalToConstant: 30),
+            menuButton.widthAnchor.constraint(equalToConstant: 30),
+            menuButton.rightAnchor.constraint(equalTo: self.rightAnchor),
+            menuButton.topAnchor.constraint(equalTo: self.topAnchor)
+        ])
+    }
+    
+    private func setDownloadButtonConstraints() {
+        downloadButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            downloadButton.heightAnchor.constraint(equalToConstant: 30),
+            downloadButton.widthAnchor.constraint(equalToConstant: 30),
+            downloadButton.rightAnchor.constraint(equalTo: self.rightAnchor),
+            downloadButtonCollapsedTopConstraint
+        ])
+    }
+    
+    private func setClearButtonConstraints() {
+        clearButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            clearButton.heightAnchor.constraint(equalToConstant: 30),
+            clearButton.widthAnchor.constraint(equalToConstant: 30),
+            clearButton.rightAnchor.constraint(equalTo: self.rightAnchor),
+            clearButtonCollapsedTopConstraint
+        ])
+    }
+    
+    private func setInfoButtonConstraints() {
+        infoButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            infoButton.heightAnchor.constraint(equalToConstant: 30),
+            infoButton.widthAnchor.constraint(equalToConstant: 30),
+            infoButton.rightAnchor.constraint(equalTo: self.rightAnchor),
+            infoButtonCollapsedTopConstraint
+        ])
+    }
 }
--- Potions-Please/Potions-Please/Views/MenuView/MenuView.swift
+++ Potions-Please/Potions-Please/Views/MenuView/MenuView.swift
@@ -9,7 +9,49 @@
 import UIKit
 
 class MenuView: UIView {
-
+    
+    enum MenuViewState {
+        case opened
+        case collapsed
+    }
+    
+    lazy var menuButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "menu-purple"), for: .normal)
+        button.addTarget(self, action: #selector(menuButtonPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    var downloadButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "menu-download-purple"), for: .normal)
+        button.alpha = 0
+        return button
+    }()
+    
+    var clearButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "menu-clear-purple"), for: .normal)
+        button.alpha = 0
+        return button
+    }()
+    
+    var infoButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named: "menu-info-purple"), for: .normal)
+        button.alpha = 0
+        return button
+    }()
+    
+    lazy var downloadButtonOpenedTopConstraint: NSLayoutConstraint = downloadButton.topAnchor.constraint(equalTo: menuButton.bottomAnchor, constant: 20)
+    lazy var downloadButtonCollapsedTopConstraint: NSLayoutConstraint = downloadButton.topAnchor.constraint(equalTo: menuButton.topAnchor)
+    
+    lazy var clearButtonOpenedTopConstraint: NSLayoutConstraint = clearButton.topAnchor.constraint(equalTo: menuButton.bottomAnchor, constant: 20)
+    lazy var clearButtonCollapsedTopConstraint: NSLayoutConstraint = clearButton.topAnchor.constraint(equalTo: downloadButton.topAnchor)
+    
+    lazy var infoButtonOpenedTopConstraint: NSLayoutConstraint = infoButton.topAnchor.constraint(equalTo: menuButton.bottomAnchor, constant: 20)
+    lazy var infoButtonCollapsedTopConstraint: NSLayoutConstraint = infoButton.topAnchor.constraint(equalTo: clearButton.topAnchor)
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.backgroundColor = .gray
@@ -19,7 +61,11 @@ class MenuView: UIView {
     }
     
     required init?(coder: NSCoder) {
-          fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    
+    @objc func menuButtonPressed() {
+        print("pressed")
+    }
 }
