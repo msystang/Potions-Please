ee63a7e35fbd446dba50919df91ac719e1243189
Sunni Tang <sunnitang@Sunnis-MacBook-Pro.local>
2020-09-09 16:35:44 -0400

    creates DollView+AsImage extension

---
+++ Potions-Please/Potions-Please/Views/DollView/DollView+AsImage.swift
@@ -0,0 +1,26 @@
+//
+//  DollView+AsImage.swift
+//  Potions-Please
+//
+//  Created by Sunni Tang on 9/9/20.
+//  Copyright © 2020 Sunni Tang. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func asImage() -> UIImage {
+        if #available(iOS 10.0, *) {
+            let renderer = UIGraphicsImageRenderer(bounds: bounds)
+            return renderer.image { rendererContext in
+                layer.render(in: rendererContext.cgContext)
+            }
+        } else {
+            UIGraphicsBeginImageContext(self.frame.size)
+            self.layer.render(in:UIGraphicsGetCurrentContext()!)
+            let image = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return UIImage(cgImage: image!.cgImage!)
+        }
+    }
+}
